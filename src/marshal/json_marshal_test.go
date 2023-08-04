c8c24f18f72f3cfd9680240391ec473eb4ff5c54
yibin <yibin.net@gmail.com>
2023-08-04 21:16:12 +0800

    add:testify/assert

--- src/marshal/json_marshal_test.go
+++ src/marshal/json_marshal_test.go
@@ -1,8 +1,11 @@
 package marshal
 
 import (
+	"encoding/json"
 	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPerson_MarshalJSON(t *testing.T) {
@@ -44,6 +47,42 @@ func TestPerson_MarshalJSON(t *testing.T) {
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("MarshalJSON() got = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestPerson_MarshalJSONWithValue(t *testing.T) {
+	type fields struct {
+		Name  string
+		Age   int
+		Phone string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "Test_MarshalJSON",
+			fields: fields{
+				Name:  "Github",
+				Age:   20,
+				Phone: "13800138000",
+			},
+			want: `{"name":"Github","age":20,"phone":"13800138000"}`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := Person{
+				Name:  tt.fields.Name,
+				Age:   tt.fields.Age,
+				Phone: tt.fields.Phone,
+			}
+			got, err := json.Marshal(p)
+			assert.NoError(t, err)
+			assert.Equalf(t, tt.want, string(got), "MarshalJSON() = %v, want %v", string(got), tt.want)
 		})
 	}
 }
